f9c0326cd813fdf5a063f2769990af6cdf39cab3
OleksandrV97 <vasyliev1work@gmail.com>
2025-03-23 12:18:42 +0100

    Commit second HorseTest

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class HorseTest {
+
+    @Test
+    public void nullNameException() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1,1));
+    }
+
+    @Test
+    public void nullNameMessage() {
+        try {
+            new Horse(null, 1, 1);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name cannot be null.",e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   ", "/t/t"})
+    public void blankNameExeption(String name) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1,1));
+
+        assertEquals("Name cannot be null.",e.getMessage());
+    }
 }
+
